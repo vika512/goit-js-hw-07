f6eb0b62742bc5181b3b87d5889cb41eba89e1d1
vika512 <120404708+vika512@users.noreply.github.com>
2023-05-01 22:15:16 -0400

    added 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,4 +4,35 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 
+const imagesList = document.querySelector('.gallery');
+
+const imagesListItems = galleryItems.map(item =>
+    `<li class="gallery__item"> 
+        <a class="gallery__link" href="${item.original}"> 
+            <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></li>`)
+    .join("");
+
+
+imagesList.innerHTML = imagesListItems;
+
+
+imagesList.addEventListener("click", onImageClick);
+
+function onImageClick (event) {
+    event.preventDefault();
+
+    if (event.target.classList.value === "gallery__image") {
+        const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+        instance.show()
+
+        imagesList.addEventListener("keydown", (event) => {
+            if(instance.visible() === true){
+                if(event.key === "Escape"){
+                    instance.close();
+                }
+            }
+        })
+    };
+};
+
 
